86f4df46d540bf426b7bef896d734065e1a191b2
johan <johanfelipe29@gmail.com>
2021-02-02 14:41:08 -0500

    EJS templates implemented & Index view complete

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const indexRoute = require('./routes/index');
 const usersRoute = require('./routes/users');
+const expressLayouts = require('express-ejs-layouts');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 
+//EJS
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+
 //Routes
 app.use('/', indexRoute);
 app.use('/users', usersRoute);
 
+
 app.listen(PORT, () => {
-    console.log(`Server running at http:localhost:${PORT}`);
+    console.log(`Server running at http://localhost:${PORT}`);
 });
